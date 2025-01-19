28525e80d04ddadb3535d24d150e1170311632ce
Yassine Ouabou <wayasyt@gmail.com>
2025-01-19 23:04:55 +0100

    fix problem of connecxion

--- src/main/java/net/projet/util/DataBaseConnection.java
+++ src/main/java/net/projet/util/DataBaseConnection.java
@@ -2,24 +2,42 @@ package net.projet.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataBaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/quiz";
     private static final String PASSWORD ="";
     private static final String USER = "root";
 
-    public static Connection getConnection(){
-        Connection connection = null;
-        try{
+    private static  Connection connection;
+
+    private DataBaseConnection(){}
+    static {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL,USER,PASSWORD);
-            if(connection!=null)
-                System.out.println("CONNECTED...");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("CONNECTED...");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver JDBC non trouvé", e);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur de connexion à la base de données", e);
         }
+    }
+
+
+    public static Connection getConnection() {
         return connection;
     }
 
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Connexion fermée.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
